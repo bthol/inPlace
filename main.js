7b4b47143f5f4ecdd47b09bbfe6212da4ae9943f
Blake <bthollaug@gmail.com>
2023-10-25 11:43:23 -0700

    indentation

--- main.js
+++ main.js
@@ -38,138 +38,139 @@ const emptyRows = [
 const storage = document.body.querySelector('#storage-container');
 
 function generateID() {
-  let id = "";
-  for (let i = 0; i < 10; i++) {
-    id += `${Math.floor(Math.random() * 11)}`;
-  }
-  return id;
+    let id = "";
+    for (let i = 0; i < 10; i++) {
+        id += `${Math.floor(Math.random() * 11)}`;
+    }
+    return id;
 };
 
 function createTray() {
-  const tray = document.createElement('div');
-  tray.classList.add("tray");
-  for (let i = 0; i < bags; i++) {
-    const bag = document.createElement('div');
-    bag.classList.add("bag");
-    bag.classList.add("bag-empty");
-    bag.setAttribute("id", generateID());
-    bag.innerText = "0";
-    tray.appendChild(bag);
-  }
-  return tray;
+    const tray = document.createElement('div');
+    tray.classList.add("tray");
+    for (let i = 0; i < bags; i++) {
+        const bag = document.createElement('div');
+        bag.classList.add("bag");
+        bag.classList.add("bag-empty");
+        bag.setAttribute("id", generateID());
+        bag.innerText = "0";
+        tray.appendChild(bag);
+    }
+    return tray;
 };
 
 function createRow() {
-  const row = document.createElement('div');
-  row.classList.add("row");
-  for (let i = 0; i < trays; i++) {
-    const tray = createTray();
-    row.appendChild(tray);
-  }
-  return row;
+    const row = document.createElement('div');
+    row.classList.add("row");
+    for (let i = 0; i < trays; i++) {
+        const tray = createTray();
+        row.appendChild(tray);
+    }
+    return row;
 };
 
 function createColumn(number) {
-  const container = document.createElement('div');
-  
-  const colNum = document.createElement('h3');
-  colNum.innerText = number;
-  colNum.style.marginBottom = 0;
-  
-  const colData = document.createElement('div');
-  colData.classList.add("col-data");
-    
-  const col = document.createElement('div');
-  col.classList.add("column");
+    const container = document.createElement('div');
 
-  container.appendChild(colNum);
-  container.appendChild(col);
+    const colNum = document.createElement('h3');
+    colNum.innerText = number;
+    colNum.style.marginBottom = 0;
 
-  return container;
+    const colData = document.createElement('div');
+    colData.classList.add("col-data");
+
+    const col = document.createElement('div');
+    col.classList.add("column");
+
+    container.appendChild(colNum);
+    // container.appendChild(colData);
+    container.appendChild(col);
+
+    return container;
 };
 
 function generateStorage() {
-  for (let i = 0; i < cols; i++) {
-    const container = createColumn(`Column #${i + 1} `);
-    const col = container.querySelector('div');
-    for (let j = 0; j < rows; j++) {
-      const row = createRow();
-      col.appendChild(row);
+    for (let i = 0; i < cols; i++) {
+        const container = createColumn(`Column #${i + 1} `);
+        const col = container.querySelector('div');
+        for (let j = 0; j < rows; j++) {
+            const row = createRow();
+            col.appendChild(row);
+        }
+        storage.appendChild(container);
     }
-    storage.appendChild(container);
-  }
 };
 generateStorage();
 
 // SYSTEM OPERATION
 function emptyRow(col, row) {
-  try {
-    storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].classList.add("empty-row");
-    
-    storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.tray').forEach((r) => {
-      r.style.visibility = "hidden";
-    });
-    for (let i = 0; i < trays; i++) {
-      for (let j = 0; j < bags; j++) {
-        storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.tray')[i].querySelectorAll('.bag')[j].classList.remove("bag-empty");
-      }
+    try {
+        storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].classList.add("empty-row");
+        storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.tray').forEach((r) => {
+            r.style.visibility = "hidden";
+        });
+        for (let i = 0; i < trays; i++) {
+            for (let j = 0; j < bags; j++) {
+                storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.tray')[i].querySelectorAll('.bag')[j].classList.remove("bag-empty");
+            }
+        }
+    } catch (error) {
+        console.log({"operation": "emptyRow", "error": error});
     }
-    
-  } catch (error) {
-    console.log({"operation": "emptyRow", "error": error});
-  }
 };
 
 emptyRow(0, 3);
 
 function emptyRowsAll(x) {
-  for (let i = 0; i < x.length; i++) {
-    emptyRow(x[i][0], x[i][1]);
-  }
+    for (let i = 0; i < x.length; i++) {
+        emptyRow(x[i][0], x[i][1]);
+    }
 };
 
 // emptyRowsAll(emptyRows);
 
 function addBags(col, row, tray) {
-  try {
-    if (!storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].classList.contains('empty-row')) {
-      const bags = storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.tray')[tray].querySelectorAll('.bag');
-      for (let i = 0; i < bags.length; i++) {
-        bags[i].classList.remove("bag-empty");
-        bags[i].classList.add("bag-full");
-        bags[i].innerText = "1";
-      }
+    try {
+        if (!storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].classList.contains('empty-row')) {
+            const bags = storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.tray')[tray].querySelectorAll('.bag');
+            for (let i = 0; i < bags.length; i++) {
+                bags[i].classList.remove("bag-empty");
+                bags[i].classList.add("bag-full");
+                bags[i].innerText = "1";
+            }
+        }
+    } catch (error) {
+        console.log({"operation": "addBags", "error": error});
     }
-  } catch (error) {
-    console.log({"operation": "addBags", "error": error});
-  }
 };
 // addBags(0, 3, 0);
 // addBags(0, 3, 1);
 // addBags(0, 3, 2);
 
 function addBagsColumn(col) {
-  for (let i = 0; i < rows; i++) {
-    addBags(col, i, 0);
-    addBags(col, i, 1);
-    addBags(col, i, 2);
-  }
+    for (let i = 0; i < rows; i++) {
+        addBags(col, i, 0);
+        addBags(col, i, 1);
+        addBags(col, i, 2);
+    }
 };
 
 addBagsColumn(0);
 
 function addBagsColumnsAll(cols) {
-  for (let i = 0; i < cols; i++) {
-    addBagsColumn(i);
-  }
+    for (let i = 0; i < cols; i++) {
+        addBagsColumn(i);
+    }
 };
 
 // addBagsColumnsAll(cols);
 
 // Column Data
-// function addColumnData(col) {
-  
-// };
+function addColumnData(col) {
+    console.log(col);
+};
+
+addColumnData(0);
 
 // System Model Settings
 const settings = document.body.querySelector('#system-model-settings');
@@ -185,4 +186,8 @@ settings.save.addEventListener("click", () => {
         document.body.style.setProperty("--scale", "3vmin");
         document.body.style.setProperty("--wrapping", "no-wrap");
     }
+});
+settings.default.addEventListener("click", () => {
+    document.body.style.setProperty("--scale", ".42vmin");
+    document.body.style.setProperty("--wrapping", "wrap");
 });
