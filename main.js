255d13074f073a7f1c9046b532fdec6128fc7a60
Blake <bthollaug@gmail.com>
2024-04-05 14:20:53 -0700

    octant integer coordinate field

--- main.js
+++ main.js
@@ -1,11 +1,15 @@
+// user interacts with forms in interface to establish defintions of space in spaceDef structure
+// definitions are used to generate spatial models in a separate structure
+// Interactions with each model are made possible by getPoint, openPoint and closePoint functions.
+
 //  SPACE
 let spaceDef = [
     {
-        name: "space-1",
-        x: 10,
-        y: 10,
+        id: "space-1",
+        x: 9,
+        y: 11,
         z: 10,
-        quadrant: 1,
+        octant: false,
         obstruct : [
             {
                 xi: 2,
@@ -18,11 +22,11 @@ let spaceDef = [
         ],
     },
     {
-        name: "space-2",
+        id: "space-2",
         x: 5,
         y: 5,
         z: 5,
-        quadrant: 4,
+        octant: true,
         obstruct : [],
     },
 ];
@@ -31,23 +35,39 @@ function modelSpaces(spaceDef) {
     let spaces = [];
     for ( let i = 0; i < spaceDef.length; i++ ) {
         let space = [];
-        if (spaceDef[i].quadrant === 1) {
-            // one quadrant coordinate field
+        if (spaceDef[i].octant === false) {
+            // positive coordinate field
             for (let x = 0; x < spaceDef[i].x; x++) {
                 for (let y = 0; y < spaceDef[i].y; y++) {
                     for (let z = 0; z < spaceDef[i].z; z++) {
                         // each coordinate point
-                        space.push({coor: [x, y, z], open: true});
+                        space.push({space: spaceDef[i].id, coor: [x, y, z], open: true});
                     }
                 }
             }
         } else {
-            // four quadrant coordinate field
+            // octant integer coordinate field
             for (let x = -(Math.ceil(spaceDef[i].x / 2)) + 1; x < Math.floor(spaceDef[i].x / 2) + 1; x++) {
                 for (let y = -(Math.ceil(spaceDef[i].y / 2)) + 1; y < Math.floor(spaceDef[i].y / 2) + 1; y++) {
                     for (let z = -(Math.ceil(spaceDef[i].z / 2)) + 1; z < Math.floor(spaceDef[i].z / 2) + 1; z++) {
                         // each coordinate point
-                        space.push({coor: [x, y, z], open: true});
+                        if (x < 0 && y >= 0 && z < 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 1});
+                        } else if (x >= 0 && y >= 0 && z < 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 2});
+                        } else if (x >= 0 && y < 0 && z < 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 3});
+                        } else if (x < 0 && y < 0 && z < 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 4});
+                        } else if (x < 0 && y >= 0 && z >= 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 5});
+                        } else if (x >= 0 && y >= 0 && z >= 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 6});
+                        } else if (x >= 0 && y < 0 && z >= 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 7});
+                        } else if (x < 0 && y < 0 && z >= 0) {
+                            space.push({space: spaceDef[i].id, coor: [x, y, z], open: true, octant: 8});
+                        }
                     }
                 }
             }
@@ -57,7 +77,7 @@ function modelSpaces(spaceDef) {
     return spaces;
 };
 
-function getCoor(model, coordinate) {
+function getPoint(model, coordinate) {
     // Each spatial model is an organized list of objects, where each object represents a point in the space modelled.
     // Their method of generation is three levels of nested iteration (cubic complexity; O(n^3)).
     
@@ -97,10 +117,19 @@ function getCoor(model, coordinate) {
     return spatialModels[model][(coordinate[0] * spaceDef[model].y * spaceDef[model].z) + (coordinate[1] * spaceDef[model].z) + coordinate[2]];
 };
 
+function openPoint(model, coordinate) {
+    spatialModels[model][(coordinate[0] * spaceDef[model].y * spaceDef[model].z) + (coordinate[1] * spaceDef[model].z) + coordinate[2]].open = true;
+};
+
+function closePoint(model, coordinate) {
+    spatialModels[model][(coordinate[0] * spaceDef[model].y * spaceDef[model].z) + (coordinate[1] * spaceDef[model].z) + coordinate[2]].open = false;
+};
+
 let spatialModels = modelSpaces(spaceDef);
 console.log(spatialModels);
-let coor1 = getCoor(0, [3, 2, 1]);
-// console.log(coor1);
+closePoint(1, [3, 2, 1]);
+let coor1 = getPoint(1, [3, 2, 1]);
+console.log(coor1);
 
 // OBJECT
 let objectDef = [
@@ -253,377 +282,3 @@ sliderInitRatio("playback-multiplier", "playback-multiplier-val");
 
 // Runtime Controls
 const formControls = panel.querySelector('#playback-controls');
-
-// OLD STUFF
-// // PARAMETERS
-// let cols = 2;
-// let rows = 2;
-// let containers = 2;
-// let slots = 2;
-// let emptyRows = [];
-
-// // SYSTEM SETUP
-// const storage = document.body.querySelector('#storage-content');
-
-// function generateID() {
-//     let id = "";
-//     for (let i = 0; i < 10; i++) {
-//         id += `${Math.floor(Math.random() * 11)}`;
-//     }
-//     return id;
-// };
-
-// function createContainer() {
-//     const container = document.createElement('div');
-//     container.classList.add("container");
-//     for (let i = 0; i < slots; i++) {
-//         const slot = document.createElement('div');
-//         slot.classList.add("slot");
-//         slot.classList.add("slot-empty");
-//         slot.setAttribute("id", generateID());
-//         slot.innerText = "0";
-//         container.appendChild(slot);
-//     }
-//     return container;
-// };
-
-// function createRow() {
-//     const row = document.createElement('div');
-//     row.classList.add("row");
-//     for (let i = 0; i < containers; i++) {
-//         const container = createContainer();
-//         row.appendChild(container);
-//     }
-//     return row;
-// };
-
-// function createColumn(number) {
-//     const container = document.createElement('div');
-
-//     const colNum = document.createElement('h3');
-//     colNum.innerText = number;
-//     colNum.style.marginBottom = 0;
-
-//     const colData = document.createElement('p');
-//     colData.classList.add("col-data");
-
-//     const col = document.createElement('div');
-//     col.classList.add("column");
-
-//     container.appendChild(colNum);
-//     container.appendChild(colData);
-//     container.appendChild(col);
-
-//     return container;
-// };
-
-// function generateStorage() {
-//     for (let i = 0; i < cols; i++) {
-//         const container = createColumn(`Column #${i + 1} `);
-//         const col = container.querySelector('div');
-//         for (let j = 0; j < rows; j++) {
-//             const row = createRow();
-//             col.appendChild(row);
-//         }
-//         storage.appendChild(container);
-//     }
-// };
-// generateStorage();
-
-// // MODEL OPERATION
-// function emptyRow(col, row) {
-//     try {
-//         storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].classList.add("empty-row");
-//         storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.container').forEach((r) => {
-//             r.style.visibility = "hidden";
-//         });
-//         for (let i = 0; i < containers; i++) {
-//             for (let j = 0; j < slots; j++) {
-//                 storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.container')[i].querySelectorAll('.slot')[j].classList.remove("slot-empty");
-//             }
-//         }
-//     } catch (error) {
-//         console.log({"operation": "emptyRow", "error": error});
-//     }
-// };
-
-// function emptyRowsAll(x) {
-//     for (let i = 0; i < x.length; i++) {
-//         emptyRow(x[i][0], x[i][1]);
-//     }
-// };
-
-// function addSlots(col, row, container) {
-//     try {
-//         if (!storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].classList.contains('empty-row')) {
-//             const slots = storage.querySelectorAll('.column')[col].querySelectorAll('.row')[row].querySelectorAll('.container')[container].querySelectorAll('.slot');
-//             for (let i = 0; i < slots.length; i++) {
-//                 slots[i].classList.remove("slot-empty");
-//                 slots[i].classList.add("slot-full");
-//                 slots[i].innerText = "1";
-//             }
-//         }
-//     } catch (error) {
-//         console.log({"operation": "addSlots", "error": error});
-//     }
-// };
-
-// function addSlotsColumn(col) {
-//     for (let i = 0; i < rows; i++) {
-//         for (let j = 0; j < containers; j++) {
-//             addSlots(col, i, j);
-//         }
-//     }
-// };
-
-// function addSlotsColumnsAll(cols) {
-//     for (let i = 0; i < cols; i++) {
-//         addSlotsColumn(i);
-//     }
-// };
-
-// // Data
-// function addColumnData(col) {
-//     // select display
-//     const display = storage.querySelectorAll('.col-data')[col];
-//     // get data
-//     const full = storage.querySelectorAll('.column')[col].querySelectorAll('.slot-full').length;
-//     const empty = storage.querySelectorAll('.column')[col].querySelectorAll('.slot-empty').length;
-//     // display data
-//     const fullSlots = document.createElement('div');
-//     fullSlots.innerText = `${full} : Slots Full`;
-
-//     const emptySlots = document.createElement('div');
-//     emptySlots.innerText = `${empty} : Slots Empty`;
-
-//     const fullRows = document.createElement('div');
-//     fullRows.innerText = `${full / (slots * containers)} : Rows Full`;
-
-//     const emptyRows = document.createElement('div');
-//     emptyRows.innerText = `${empty / (slots * containers)} : Rows Empty`;
-
-//     display.appendChild(fullSlots);
-//     display.appendChild(emptySlots);
-//     display.appendChild(fullRows);
-//     display.appendChild(emptyRows);
-// };
-
-// function addColumnDataAll(cols) {
-//     for (let i = 0; i < cols; i++) {
-//         addColumnData(i);
-//     }
-// };
-
-// function updateModelDisplay() {
-//     // empty
-//     emptyRowsAll(emptyRows);
-//     // add
-//     addSlotsColumnsAll(cols);
-//     addColumnDataAll(cols);
-// };
-// updateModelDisplay();
-
-// // Model Parameters
-// const parameters = document.body.querySelector('#system-model-parameters');
-// function listenRemoveForm(button, formID) {
-//     function removeForm() {
-//         const form = document.querySelector(`#${formID}`);
-//         form.deleteFormButton.removeEventListener("click", removeForm);
-//         form.remove();
-//     }
-//     button.addEventListener("click", removeForm);
-// };
-// let emptyColFormID = 0;
-// parameters.addEmptyColButton.addEventListener('click', () => {
-//     const form = document.createElement('form');
-//     form.setAttribute('action', './');
-//     form.setAttribute('method', 'POST');
-//     form.setAttribute('class', `empty-col-form`);
-//     form.setAttribute('id', `empty-col-form-${emptyColFormID}`);
-//     const formID = `empty-col-form-${emptyColFormID}`;
-    
-//     const formLabel = document.createElement('label');
-//     formLabel.innerText = "Column ";
-    
-//     const inputColumn = document.createElement('input');
-//     inputColumn.setAttribute('name', 'inputColumn');
-//     inputColumn.setAttribute('class', 'input-column');
-//     inputColumn.setAttribute('type', 'number');
-//     inputColumn.setAttribute('value', '1');
-//     inputColumn.setAttribute('min', '1');
-//     inputColumn.setAttribute('max', `${parameters.columns.value}`);
-//     inputColumn.style.width = "50px";
-    
-//     const deleteFormButton = document.createElement('button');
-//     deleteFormButton.setAttribute('type', 'button');
-//     deleteFormButton.setAttribute('name', "deleteFormButton");
-//     deleteFormButton.innerText = "remove";
-//     listenRemoveForm(deleteFormButton, formID);
-    
-//     form.appendChild(formLabel);
-//     form.appendChild(inputColumn);
-//     form.appendChild(deleteFormButton);
-//     parameters.querySelector('#empty-col-form-container').appendChild(form);
-//     emptyColFormID += 1;
-// });
-// let emptyRowFormID = 0;
-// parameters.addEmptyRowButton.addEventListener('click', () => {
-//     const form = document.createElement('form');
-//     form.setAttribute('action', './');
-//     form.setAttribute('method', 'POST');
-//     form.setAttribute('class', `empty-row-form`);
-//     form.setAttribute('id', `empty-row-form-${emptyRowFormID}`);
-//     const formID = `empty-row-form-${emptyRowFormID}`;
-    
-//     const formLabel = document.createElement('label');
-//     formLabel.innerText = "Column/Row ";
-    
-//     const inputColumn = document.createElement('input');
-//     inputColumn.setAttribute('name', 'inputColumn');
-//     inputColumn.setAttribute('class', 'input-column');
-//     inputColumn.setAttribute('type', 'number');
-//     inputColumn.setAttribute('value', '1');
-//     inputColumn.setAttribute('min', '1');
-//     inputColumn.setAttribute('max', `${parameters.columns.value}`);
-//     inputColumn.style.width = "50px";
-    
-//     const inputRow = document.createElement('input');
-//     inputRow.setAttribute('name', 'inputRow');
-//     inputRow.setAttribute('class', 'input-row');
-//     inputRow.setAttribute('type', 'number');
-//     inputRow.setAttribute('value', '1');
-//     inputRow.setAttribute('min', '1');
-//     inputRow.setAttribute('max', `${parameters.rows.value}`);
-//     inputRow.style.width = "50px";
-    
-//     const deleteFormButton = document.createElement('button');
-//     deleteFormButton.setAttribute('type', 'button');
-//     deleteFormButton.setAttribute('name', "deleteFormButton");
-//     deleteFormButton.innerText = "remove";
-//     listenRemoveForm(deleteFormButton, formID);
-    
-//     form.appendChild(formLabel);
-//     form.appendChild(inputColumn);
-//     form.appendChild(inputRow);
-//     form.appendChild(deleteFormButton);
-//     parameters.querySelector('#empty-row-form-container').appendChild(form);
-//     emptyRowFormID += 1;
-// });
-
-// // dynamcially update empty row input maximums based on current column and row parameters
-// parameters.columns.addEventListener("input", () => {
-//     parameters.querySelectorAll('.input-column').forEach((column) => {
-//         column.setAttribute('max', `${parameters.columns.value}`);
-//         if (Number(column.value) > Number(parameters.columns.value)) {
-//             column.value = parameters.columns.value;
-//         }
-//     })
-// });
-// parameters.rows.addEventListener("input", () => {
-//     parameters.querySelectorAll('.input-row').forEach((row) => {
-//         row.setAttribute('max', `${parameters.rows.value}`);
-//         if (Number(row.value) > Number(parameters.rows.value)) {
-//             row.value = parameters.rows.value;
-//         }
-//     })
-// });
-
-// // parameter form button listeners
-// parameters.save.addEventListener("click", () => {
-//     // Update Model Parameters
-//     cols = parameters.columns.value;
-//     rows = parameters.rows.value;
-//     containers = parameters.containers.value;
-//     slots = parameters.slots.value;
-
-//     // intialize empty structures
-//     emptyCols = [];
-//     emptyRows = [];
-//     emptyContainers = [];
-//     emptySlots = [];
-
-//     // reassign to empty structures
-//     parameters.querySelectorAll('.empty-col-form').forEach((form) => {
-//         emptyCols.push(Number(form.inputColumn.value) - 1);
-//     });
-//     parameters.querySelectorAll('.empty-row-form').forEach((form) => {
-//         emptyRows.push([Number(form.inputColumn.value) - 1, Number(form.inputRow.value) - 1]);
-//     });
-
-//     // Regenerate Model
-//     storage.innerHTML = '';
-//     generateStorage();
-//     updateModelDisplay();
-
-//     // Model Display Rescaling
-//     if (parameters.scale.value === "small") {
-//         document.body.style.setProperty("--scale", ".42vmin");
-//     } else if (parameters.scale.value === "medium") {
-//         document.body.style.setProperty("--scale", "1.5vmin");
-//     } else if (parameters.scale.value === "large") {
-//         document.body.style.setProperty("--scale", "3vmin");
-//     }
-// });
-
-// parameters.default.addEventListener("click", () => {
-//     // Update Model Parameters
-//     cols = 2;
-//     rows = 2;
-//     containers = 2;
-//     slots = 2;
-//     emptyRows = [];
-    
-//     // Regenerate Model
-//     parameters.querySelector('#empty-row-form-container').innerHTML = '';
-//     storage.innerHTML = '';
-//     generateStorage();
-//     updateModelDisplay();
-
-//     // Model Display Rescaling
-//     document.body.style.setProperty("--scale", ".42vmin");
-// });
-
-// // playback speed slider display
-// const playback = document.body.querySelector('#playback-rate');
-// const playVal = document.body.querySelector('#playback-rate-val');
-
-// playVal.textContent = `0${playback.value}`;
-
-// playback.addEventListener('dblclick', () => {
-//     playback.value = 50;
-//     playVal.textContent = `0${playback.value}`;
-// });
-
-// playback.addEventListener('input', () => {
-//     if (playback.value.length === 3) {
-//         playVal.textContent = playback.value;
-//     } else if (playback.value.length === 2) {
-//         playVal.textContent = `0${playback.value}`;
-//     } else {
-//         playVal.textContent = `00${playback.value}`;
-//     }
-// });
-
-// // data resolution slider display
-// const resolution = document.body.querySelector('#data-resolution');
-// const resVal = document.body.querySelector('#data-resolution-val');
-
-// resVal.textContent = `0${resolution.value}`;
-
-// resolution.addEventListener('dblclick', () => {
-//     resolution.value = 50;
-//     resVal.textContent = `0${resolution.value}`;
-// });
-
-// resolution.addEventListener('input', () => {
-//     if (resolution.value.length === 3) {
-//         resVal.textContent = resolution.value;
-//     } else if (resolution.value.length === 2) {
-//         resVal.textContent = `0${resolution.value}`;
-//     } else {
-//         resVal.textContent = `00${resolution.value}`;
-//     }
-// });
-
-// // generate graph
-// const graph = document.body.querySelector('#graph-model-change-over-time');
